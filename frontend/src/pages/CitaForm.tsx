d7c1233fe0c30727165f92604abcf25e3c12a7ee
Mauricio <991573068gem@gmail.com>
2025-08-22 12:45:45 -0500

    agregar el useeffect para cargar a los pacientes

--- frontend/src/pages/CitaForm.tsx
+++ frontend/src/pages/CitaForm.tsx
@@ -11,4 +11,11 @@ const CitaForm: React.FC = () => {
     const [hora, setHora] = useState("");
     const [pacienteId, setPacienteId] = useState<number | null>(null);
     const [error, setError] = useState("");
+
+    useEffect(()=> { //el useEffect: asistente que hace tareas al terminar el renderizado
+        fetch("api/pacientes") //llamada a la API
+        .then((response) => response.json()) //convierte la respuesta en JSON
+        .then((data) => setPacientes(data)) //establece los pacientes en el estado
+        .catch(error => console.error("Error al cargar los pacientes:", error)); //manejo
+    },[]);
 };
